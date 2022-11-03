33876f8f121d7c20c71c9a1993f95d250460a3d0
BiliOrange <liailio3@outlook.com>
2022-11-03 11:24:35 +0800

    all

---
+++ dledit.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+int main()
+{
+	cout<<"Downloading Orange Game System edit tool files, please be patient......";
+	Sleep(10000);
+	return 0;
+}
